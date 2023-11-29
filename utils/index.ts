2b3fd93ff4e38172d5f316dcda9100bad3d891b2
Jun Ukemori <jukemori@gmail.com>
2023-11-29 22:56:18 +0900

    add car card

---
+++ utils/index.ts
@@ -0,0 +1,12 @@
+export async function fetchCars() {
+  const headers = {
+    'X-RapidAPI-Key': process.env.RAPIDAPI_KEY!,
+    'X-RapidAPI-Host': process.env.RAPIDAPI_HOST!
+  }
+
+  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', { headers });
+
+  const result = await response.json();
+
+  return result;
+}
