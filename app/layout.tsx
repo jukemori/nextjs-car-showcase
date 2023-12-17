b79eafb54f820c70595a70044519363c399c96e7
Jun Ukemori <jukemori@gmail.com>
2023-12-17 21:18:56 +0900

    update page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
-import { Footer, Navbar } from "@/components";
 import "./globals.css";
-import type { Metadata } from "next";
 
-export const metadata: Metadata = {
+import { Footer, Navbar } from "@/components";
+
+export const metadata = {
   title: "Car Hub",
-  description: "Discover the best cars in the world.",
+  description: "Discover world's best car showcase application",
 };
 
 export default function RootLayout({
